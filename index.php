3b15c760fef687cf85358fa2957748f1aeacf9cb
rathanont <rathanont.s@gmail.com>
2019-03-31 09:01:29 +0700

    update

--- index.php
+++ index.php
@@ -1,45 +1,5 @@
-<!DOCTYPE html>
-<html lang="th" class="h-100">
-
-<head>
-    <meta charset="UTF-8">
-    <title>ROHAN</title>
-    <link rel="icon" href="./favicon.ico">
-    <link rel="stylesheet" href="./dist/css/style.css">
-</head>
-
-<body class="landing-page d-flex flex-column h-100">
-    <header class="container">
-        <nav class="nav justify-content-between align-items-center text-center">
-            <a href="#" class="nav-link active">
-                <div>หน้าหลัก</div>
-                <small>Home</small>
-            </a>
-            <a href="#" class="nav-link">
-                <div>ข่าวสาร</div>
-                <small>News</small>
-            </a>
-            <a href="#" class="nav-link">
-                <div>แนะนำผู้เล่น</div>
-                <small>Game Guide</small>
-            </a>
-            <a href="./index.html" class="nav-link nav-link-logo">
-                <img src="./dist/images/LOGO.png" alt="ROHAN">
-            </a>
-            <a href="#" class="nav-link">
-                <div>ดาวน์โหลด</div>
-                <small>Download</small>
-            </a>
-            <a href="#" class="nav-link">
-                <div>ไอเท็มมอล</div>
-                <small>R-Mall</small>
-            </a>
-            <a href="#" class="nav-link">
-                <div>ระบบสมาชิก</div>
-                <small>Member Login</small>
-            </a>
-        </nav>
-    </header>
+<!-- Include header -->
+<?php include('./include/header.php') ?>
 
     <main class="flex-shrink-0 container">
         <!-- Register -->
@@ -570,42 +530,5 @@
         </div>
     </main>
 
-    <footer class="mt-auto bg-dark pt-1 pb-4">
-        <div class="container">
-            <nav class="nav justify-content-between align-items-center text-center mb-5">
-                <a href="#" class="nav-link active">
-                    <div>หน้าหลัก</div>
-                    <small>Home</small>
-                </a>
-                <a href="#" class="nav-link">
-                    <div>ข่าวสาร</div>
-                    <small>News</small>
-                </a>
-                <a href="#" class="nav-link">
-                    <div>แนะนำผู้เล่น</div>
-                    <small>Game Guide</small>
-                </a>
-                <a href="#" class="nav-link">
-                    <div>ดาวน์โหลด</div>
-                    <small>Download</small>
-                </a>
-                <a href="#" class="nav-link">
-                    <div>ไอเท็มมอล</div>
-                    <small>R-Mall</small>
-                </a>
-                <a href="#" class="nav-link">
-                    <div>ระบบสมาชิก</div>
-                    <small>Member Login</small>
-                </a>
-            </nav>
-            <div class="text-center">
-                <button type="button" class="btn">Privacy Policy | กฏกติกา</button>
-            </div>
-        </div>
-    </footer>
-
-    <script src="./dist/js/jquery.min.js"></script>
-    <script src="./dist/js/bootstrap.bundle.min.js"></script>
-</body>
-
-</html>
+   <!-- Include footer -->
+<?php include('./include/footer.php') ?>
